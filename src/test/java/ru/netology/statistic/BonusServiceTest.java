8c0889cfe7be695abc32c3ce525a741b997a4fb7
Kirillova Olesya <olesyak-85@mail.ru>
2021-08-28 17:33:25 +0300

    initial commit

---
+++ src/test/java/ru/netology/statistic/BonusServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BonusServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    void myTest(String test, long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+}
+
+
